3e6bfd17c72772cf1751b7219a34aaca46ccfc92
Michał <micpop19@gmail.com>
2022-09-29 22:41:55 +0200

    feat: obtain data from google sheet

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,17 @@
+import requests
 
+SHEETY_ENDPOINT = "SHEETY API"
+
+
+class DataManager:
+    #class is response to connect with Google sheet
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests(url=SHEETY_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    
