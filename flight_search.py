07326c99ed273fd161e713ffb041eb505f617a70
Michał <micpop19@gmail.com>
2022-10-10 16:37:09 +0200

    feat(api): get a response with price of every flight in Google Sheet in a given time period

--- flight_search.py
+++ flight_search.py
@@ -1,12 +1,54 @@
-#import requests
+import requests
+from flight_data import FlightData
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
 
 
 class FlightSearch:
-
+    #This class is responsible for talking to the Flight Search API.
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
+
+    def check_flight_price(self, city_from_code, city_to_code, from_time, to_time):
+        search_endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": city_from_code,
+            "fly_to": city_to_code,
+            "date_from": from_time,
+            "date_to": to_time,
+            "nights_in_dst_from": 3,
+            "nights_in_dst_to": 7,
+            "adults": 1,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "curr": "PLN",
+            "max_stopovers": 0,
+        }
+        response = requests.get(url=search_endpoint, headers=headers, params=query)
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {city_to_code}")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["cityCodeFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["cityCodeTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_arrival"].split("T")[0],
+            airline=data["route"][0]["airline"],
+        )
+        print(f"{flight_data.destination_city}: {flight_data.price} PLN")
+        return flight_data
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "Your City IATA Code"
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+    data_manager.update_iatacodes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+in_six_months = datetime.now() + timedelta(days=183)
+
+for destination in sheet_data:
+    flight = flight_search.check_flight_price(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow.strftime("%d/%m/%Y"),
+        to_time=in_six_months.strftime("%d/%m/%Y"),
+    )
+    
